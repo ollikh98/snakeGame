3926652e0de07d2ecfecde81936a94c50623da2d
Oliver Husby <oliver.husby@tietoevry.com>
2024-08-01 14:59:08 +0200

    updated movement logic

--- modules/GamePiece.py
+++ modules/GamePiece.py
@@ -1,17 +1,24 @@
 import pygame
 import numpy as np
 from modules.Board import Board
+from copy import deepcopy
 
+# DIRECTION CONSTANTS
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
 
 class GamePiece(pygame.sprite.Sprite):
     def __init__(self, board: Board, screen, gamePos, size, next=None):
         self.gamePos = gamePos
         self.screenPos = board.gameToScreenCord(self.gamePos)
         self.screen = screen
-        self.dir = (1, 0)
+        self.dir = LEFT
         self.board = board
         self.color = 'green'
         self.size = size
+        self.canMove = True
         self.movementTicker = 0
         self.rect = pygame.Rect(self.screenPos[0], self.screenPos[1], size, size)
         if not next:
@@ -28,21 +35,25 @@ class GamePiece(pygame.sprite.Sprite):
 
     def move(self):
         keys = pygame.key.get_pressed()
-        prevDir = self.dir
         prevX, prevY = self.gamePos
         newX, newY = self.gamePos
 
-        if keys[pygame.K_LEFT]:
-            self.dir = (-1, 0)
-        if keys[pygame.K_RIGHT]:
-            self.dir = (1, 0)
-        if keys[pygame.K_DOWN]:
-            self.dir = (0, 1)
-        if keys[pygame.K_UP]:
-            self.dir = (0, -1)
+        if keys[pygame.K_LEFT] and self.dir != RIGHT:
+            self.dir = LEFT
+        if keys[pygame.K_RIGHT] and self.dir != LEFT:
+            self.dir = RIGHT
+        if keys[pygame.K_UP] and self.dir != DOWN:
+            self.dir = UP
+        if keys[pygame.K_DOWN] and self.dir != UP:
+            self.dir = DOWN
 
-        if not True in keys and self.movementTicker>=10:  # no key pressed
+        # no key pressed and time to move
+        if self.movementTicker >= 10:
+            self.canMove = True
+
+        if self.canMove:
             newX, newY = tuple(np.add(self.gamePos, self.dir))
+            self.canMove = False
             self.movementTicker = 0
 
         self.movementTicker += 1
